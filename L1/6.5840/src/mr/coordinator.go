743c2c734aa9aa633603f4d710779c74c7e30f74
Luke Roche <LukeRocheDev@gmail.com>
2024-06-16 12:33:19 -0500

    setup map tasks

--- L1/6.5840/src/mr/coordinator.go
+++ L1/6.5840/src/mr/coordinator.go
@@ -1,33 +1,40 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	inputFiles   []string
+	reducedFiles []string
+	outputFiles  []string
+	numReduce    int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
+func (c *Coordinator) Task(args *MapTaskArgs, reply *TaskReply) error {
+	reply.doMap = true
+	reply.file = c.inputFiles[0]
+	reply.writeTo = "test.txt"
+	return nil
+
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +48,24 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
-
+	ret = len(c.inputFiles) == 0 && len(c.reducedFiles) == c.numReduce
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{files, make([]string, 0), make([]string, 0), nReduce}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
